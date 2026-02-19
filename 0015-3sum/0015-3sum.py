44fd84608ee0c8de504757962060e09d4b0c0bc2
aks662 <158719091+aks662@users.noreply.github.com>
2026-02-19 12:52:38 +0530

    Time: 677 ms (29.97%), Space: 24.6 MB (5.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,69 +1,69 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         '''hashing'''
-        # curr_sum=[]
-        # nums=sorted(nums)
-        # n=len(nums)
+        ans=set()
+        nums=sorted(nums)
+        n=len(nums)
 
-        # for i in range(0,n):
-        #     if nums[i]>0:
-        #         break
-        #     if i>0 and nums[i]==nums[i-1]:
-        #         continue
+        for i in range(0,n-2):
+            if nums[i]>0:
+                break
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             
-        #     seen=set()
-        #     for j in range(i+1,n):
-        #         if nums[j]==nums[j-1]:
-        #             continue
-        #         k=-(nums[i]+nums[j])
-        #         if k in seen:
-        #             curr_sum.append([nums[i],k,nums[j]])
-                    
-        #             while j + 1 < n and nums[j] == nums[j+1]: #checking for duplicated for value of k and skipping them
-        # 
-        #                     j += 1
+            seen=set()
+            for j in range(i+1,n):
+                
+                # if j>i+1 and  nums[j]==nums[j-1]:
+                #     continue
+                
+                k=-(nums[i]+nums[j])
+                
+                if k in seen:
+                    ans.add((nums[i],k,nums[j]))
                             
-        #         else:
-        #             seen.add(nums[j])
-        # print(curr_sum)
+                else:
+                    seen.add(nums[j])
+        return ([list(t) for t in ans])
+
 
 
 
         '''2_pointers'''
-        nums=sorted(nums)
-        ans=[]
-        n=len(nums)
-        for i in range(n):
-            if nums[i]>0:
-                break
-            if i>0 and nums[i]==nums[i-1]: 
-                continue
+        # nums=sorted(nums)
+        # ans=[]
+        # n=len(nums)
+        # for i in range(n):
+        #     if nums[i]>0:
+        #         break
+        #     if i>0 and nums[i]==nums[i-1]: 
+        #         continue
             
-            target=-(nums[i])
-            l=i+1
-            r=n-1
+        #     target=-(nums[i])
+        #     l=i+1
+        #     r=n-1
 
-            while l<r:
+        #     while l<r:
                     
-                curr_sum=nums[l]+nums[r]
+        #         curr_sum=nums[l]+nums[r]
                 
-                if curr_sum==target:
+        #         if curr_sum==target:
                     
-                    ans.append([nums[i],nums[l],nums[r]])
-                    l+=1
-                    r-=1   
-                    while l<r and nums[l]==nums[l-1]:
-                        l+=1
-                    while r>l and nums[r]==nums[r+1]:
-                        r-=1
+        #             ans.append([nums[i],nums[l],nums[r]])
+        #             l+=1
+        #             r-=1   
+        #             while l<r and nums[l]==nums[l-1]:
+        #                 l+=1
+        #             while r>l and nums[r]==nums[r+1]:
+        #                 r-=1
                         
-                elif curr_sum<target:
-                    l+=1
-                else:
-                    r-=1
+        #         elif curr_sum<target:
+        #             l+=1
+        #         else:
+        #             r-=1
                         
                     
                     
                     
-        return (ans)
+        # return (ans)
         
